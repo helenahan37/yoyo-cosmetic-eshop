4f04dbcb89119edde7303328d05cfeb1342acf8e
Helena.Han <hlyhelena37@gmail.com>
2024-01-24 22:58:46 +1100

    add gitignore

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc2ebf47a60200f290da3a2a34098c24dc429bd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2071a29064a0f75b707e07ee0d9a3409932bcde")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
